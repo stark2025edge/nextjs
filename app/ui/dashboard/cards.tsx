9346d43162e4f3a47ee3f4ab177fb84e19960395
star2025edge <sumit.raturi@starkedge.com>
2025-03-18 19:05:14 +0530

    initial commit

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -6,6 +6,13 @@ import {
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from '@/app/lib/data';
+
+
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -14,18 +21,27 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const revenue = await fetchRevenue();
+const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
+const {
+  numberOfInvoices,
+  numberOfCustomers,
+  totalPaidInvoices,
+  totalPendingInvoices,
+} = await fetchCardData(); // wait for fetchLatestInvoices() to finish
+
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
